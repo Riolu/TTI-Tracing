4ecc135f3d248e58c6c61525b48e9c39f1d063fa
Riolu <1226097512@qq.com>
2018-08-08 19:39:29 +0800

    modify __init__.py

--- __init__.py
+++ __init__.py
@@ -3,9 +3,20 @@
 
 import os
 import sys
+from setuptools import setup
 
 
 if len(sys.argv) == 1:
     sys.argv.append("py2exe")
 
 directory = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    zipfile = None,  # 不生成library.zip文件
+    windows = [
+        {
+            "script": directory + "\\interface_multiple.py"
+            #"icon_resources":[(1, directory+"\\masterball.ico")]
+        }
+    ],
+)
